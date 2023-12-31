8e929a8b53427d447b15e6abb573dda25b3daa3c
RobinBajaj123 <RobinBajaj@Gmail.com>
2023-12-30 22:21:44 -0500

    basic conditional rendering example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 1,
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {user.imageSize > 90 ? (
+        <>
+          <h1> hello will show image</h1>
+          <img
+            src={user.imageUrl}
+            alt={'Photo of' + user.name}
+            width={user.imageSize}
+            style={{
+              borderRadius: '50%',
+              border: '2px solid black',
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <h1> hello </h1>
+          <h3> I will not show image just the message </h3>
+        </>
+      )}
+    </>
+  )
 }
 
-export default App;
+function MyButton() {
+  return <button>I am a button component</button>
+}
+
+export default App
